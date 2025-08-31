944e92c4b8e30e001625c0c01b3a82778efa0855
Plamen Vasilev <plamen.v2.1.0@gmail.com>
2025-08-31 21:18:21 +0300

    Small changes

--- db/scripts/02.data.sql
+++ db/scripts/02.data.sql
@@ -1,5 +1,5 @@
 
 INSERT INTO players(login, nickname, password)
-VALUES ('player_1', 'DOOM', '$2a$10$Ru5l9Jej8QD68G6QUyfZxOv8EZNdyQzZpei98yJ2z1ci8nZkWM7.K'),
-       ('player_2', 'The One', '$2a$10$Ql5R/kANh91.BjF6vsipmeGEABTNPPtpd0rgJqtFKCuA5XgzF6jgK');
+VALUES ('player_1', 'Player 1', '$2a$10$/mn.JzGbYyucXVG92dofwenk1IZWS7baYQ2DlBId.Y7gEOWzX5V7W'),
+       ('player_2', 'Player 2', '$2a$10$NEMdWFyvzB8NTLg.917hGeknZRy30Gpfx.TwhuFdq6.2HXGw9jTOG');
 
